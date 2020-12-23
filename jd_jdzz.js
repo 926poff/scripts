ecf080eefaa1b2faf1a60991d4ce3fe84c1391e5
fengwcarl <fengwcarl@qq.com>
2020-12-23 15:25:59 +0800

    Update jd_jdzz.js

--- jd_jdzz.js
+++ jd_jdzz.js
@@ -45,9 +45,8 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const inviteCodes = [
-  `ATGEC3-fsrn13aiaEqiM@AUWE5maSSnzFeDmH4iH0elA@ATGEC3-fsrn13aiaEqiM@AUWE5m6WUmDdZC2mr1XhJlQ@AUWE5m_jEzjJZDTKr3nwfkg@A06fNSRc4GIqY38pMBeLKQE2InZA@AUWE5mf7ExDZdDmH7j3wfkA@AUWE5m6jBy2cNAWX7j31Pxw@AUWE5mK2UnDddDTX61S1Mkw@AUWE5mavGyGZdWzP5iCoZwQ`,
-  `ATGEC3-fsrn13aiaEqiM@AUWE5maSSnzFeDmH4iH0elA@ATGEC3-fsrn13aiaEqiM@AUWE5m6WUmDdZC2mr1XhJlQ@AUWE5m_jEzjJZDTKr3nwfkg@A06fNSRc4GIqY38pMBeLKQE2InZA@AUWE5m6_BmTUPAGH42SpOkg@AUWE53NTIs3V8YBqthQMI`
-]
+  `A3o7hSQgCGp-83eBHC__Q@AUWE5m6-VyTAADjar1H1JnQ@AUWE5zurQjWZXbBarphUv@AUWE5_9nGp1FsSCeipy02@AUWE5mqiXyT0KDGeu33gYnA@AUWE5m6yVzzwJDmT_3HsZwQ@AUWE5x-_at3RKYDuQiTYI@AUWE53fnRkW5cfSinnyMY
+`]
 !(async () => {
   $.tuanList = []
   await requireConfig();
