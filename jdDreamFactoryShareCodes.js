cf1569381cf6b220f314e3019eeb15bae8db5387
fengwcarl <fengwcarl@qq.com>
2021-04-17 11:01:50 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -7,8 +7,13 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-  '2HVCZsb25UVOsaRlBI9oZA==@V5LkjP4WRyjeCKR9VRwcRX0bBuTz7MEK0-E99EJ7u0k=@Bo-jnVs_m9uBvbRzraXcSA==@-OvElMzqeyeGBWazWYjI1Q==',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '2HVCZsb25UVOsaRlBI9oZA==@1uzRU5HkaUgvy0AB5Q9VUg==@PDPM257r_KuQhil2Y7koNw==@-OvElMzqeyeGBWazWYjI1Q==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'q0rfbhmQUDwdq2wwJPSMtw==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@Sce1eufmFjlr9N2WQ57zUg==',
+  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw=='
 ]
 // 判断github action里面是否有京喜工厂互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
