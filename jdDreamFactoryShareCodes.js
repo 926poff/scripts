72716e90b4d8e9e9db3de7548644d35c629c4848
fengwcarl <fengwcarl@qq.com>
2021-05-04 18:31:13 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -7,13 +7,13 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-  'ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
-  'q0rfbhmQUDwdq2wwJPSMtw==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
-  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
-  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
-  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
-  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@Sce1eufmFjlr9N2WQ57zUg==',
-  'q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw=='
+  'EbvSErvecKT5Q8b3sqfapA==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'EbvSErvecKT5Q8b3sqfapA==@q0rfbhmQUDwdq2wwJPSMtw==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'EbvSErvecKT5Q8b3sqfapA==@q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'EbvSErvecKT5Q8b3sqfapA==@q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'EbvSErvecKT5Q8b3sqfapA==@q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@YnErutLXt2TbumITLd9sLw==@Sce1eufmFjlr9N2WQ57zUg==',
+  'EbvSErvecKT5Q8b3sqfapA==@q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@Sce1eufmFjlr9N2WQ57zUg==',
+  'EbvSErvecKT5Q8b3sqfapA==@q0rfbhmQUDwdq2wwJPSMtw==@ufujQGhAcqxdmcJG3ILdJQ==@mzK3oEDXJl5C002BElye0g==@x6Qjgvl2HlFJFdIXCLtaaQ==@KvBRvMI8K2UNk2E4Kxq35w==@YnErutLXt2TbumITLd9sLw=='
 ]
 // 判断github action里面是否有京喜工厂互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
